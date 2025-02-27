1a87e75daa3b8725abb932ce6e5bce41141b3be8
Peter Waher <peterwaher@hotmail.com>
2025-02-27 11:26:49 -0300

    Fix

--- Root/MarkdownEditorDemo/DynamicEditors.js
+++ Root/MarkdownEditorDemo/DynamicEditors.js
@@ -4,6 +4,6 @@ function AddEditor()
 {
 	var Editors = document.getElementById("Editors");
 	var Div = document.createElement("DIV");
-	Div.innerHTML = CreateHTMLDynamicEditor(++NrEditors);
+	Div.innerHTML = CreateHTMLDynamicEditor({ Nr: ++NrEditors });
 	Editors.appendChild(Div);
 }
